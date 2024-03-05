e11e48a287fa4f2f3ca3b4edcd11f95ed3d4031f
KoreanBeaver <109877122+KoreanBeaver@users.noreply.github.com>
2024-03-05 23:44:30 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+const newColourBtnElement = document.getElementById(
+    'new-colour-button'
+    );
+
+const currentColourElement = document.getElementById(
+    'current-colour'
+);
+
+const hexValues = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+];
+
+function getRandomHexValue() {
+    const getRandomIndexPosition = Math.floor(
+        Math.random() * hexValues.length
+        );
+
+    const getRandomHexValue = hexValues[getRandomIndexPosition];
+
+    return getRandomHexValue;
+}
+
+function getRandomHexString(stringLength) {
+    let hexString = '';
+    for (let i=0; i< stringLength; i++) {
+        hexString += getRandomHexValue();
+    }
+
+    return hexString;
+}
+
+newColourBtnElement.addEventListener('click', function() {
+    const RandomHexString = '#' + getRandomHexString(6);
+    
+    document.body.style.setProperty(
+        'background-color',
+         RandomHexString
+         );
+    currentColourElement.textContent = RandomHexString;
+});
